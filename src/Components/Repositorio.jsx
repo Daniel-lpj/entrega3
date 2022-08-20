2013222e88071d2fd8c7a8a0bf888b994e842a00
djunior <djunior@magnasistemas.com.br>
2022-08-19 23:04:53 -0300

    consulta de usuários github

---
+++ src/Components/Repositorio.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const Repositorio = () => {
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+          await fetch("http://api.github.com/users")
+          .then((data) => data.json())
+          .then((resp) => setUsuarios(resp))
+        })();
+    }, [])
+
+    return (
+        <>
+            <h1>Lista de usuários no Github</h1>
+            <ul>
+                {usuarios.map((usuario, index) => 
+                    <li key={index}>{usuario.login}</li>
+                )}
+            </ul>
+        </>
+    )
+}
+export default Repositorio;
